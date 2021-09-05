0d42b8a212db52d287b63eea8c0c431df981654f
vansi1804 <65750930+vansi1804@users.noreply.github.com>
2021-09-05 12:28:59 +0700

    Tang Num le thanh 8

--- src/main/java/com/mycompany/bai7_bangcuuchuong/BangLuyThua.java
+++ src/main/java/com/mycompany/bai7_bangcuuchuong/BangLuyThua.java
@@ -2,10 +2,11 @@ package com.mycompany.bai7_bangcuuchuong;
 
 public class BangLuyThua {
     public static void main(String[] args) {
-        int Num = 6;
+        //int Num = 6;
+        int Num = 8;
         for (int i = 1; i <= Num; i++) {
             for (int j = 1; j <= Num; j++) {
-                System.out.print(j+"^"+i+" = "+Math.pow(i,j)+"\t");
+                System.out.print(j+"^"+i+" = "+(Double)Math.pow(i,j)+"\t");
             }
             System.out.println();
         }
